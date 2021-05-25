ad504f8dc1fac44ac677af52fa3b052838b5459a
hishamcse <1805004@ugrad.cse.buet.ac.bd>
2021-05-25 09:00:56 +0600

    Setup done for heroku

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,7 @@ const clearField = () => {
 };
 
 const fetchData = (location) => {
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         if (!response.ok) {
             clearField();
             message2.textContent = response.error().statusText;
